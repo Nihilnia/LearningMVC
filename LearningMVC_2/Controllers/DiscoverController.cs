ecee3c3148571ba27454c5046feb2128f716116d
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-07-21 03:17:03 +0300

    2

---
+++ LearningMVC_2/Controllers/DiscoverController.cs
@@ -0,0 +1,26 @@
+﻿using LearningMVC_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningMVC_2.Controllers
+{
+    public class DiscoverController : Controller
+    {
+        // GET: Discover
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Deneme(int getID, string getName)
+        {
+            //https://localhost:44301/Discover/Deneme?id=3475634&nick=Nihil
+
+            User user01 = new User {userID = getID, userName = getName};
+            return View(user01);
+        }
+    }
+}
---
+++ LearningMVC_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LearningMVC_2.MvcApplication" Language="C#" %>
