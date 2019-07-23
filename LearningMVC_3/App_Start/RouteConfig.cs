9c27d0d49a104dddb6277f6560974bc7193435a4
Nihil <35506793+Nihilnia@users.noreply.github.com>
2019-07-23 23:28:00 +0300

    3

---
+++ LearningMVC_3/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace LearningMVC_3
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "HomePage",
+                url: "",
+                defaults: new { controller = "HomePage", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "EmployeeList",
+                url: "Employee/List/{OrderBy}/{Page}",
+                defaults: new { controller = "Employee", action = "EmployeeList" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ LearningMVC_3/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningMVC_3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeList(string OrderBy, int Page)
+        {
+            return Content("OrderBy: " + OrderBy + " Page: " + Page);
+        }
+    }
+}   
